8df4b90adc3af722b63cf0e7a196004e55bf8db0
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-08-08 20:17:54 +0530

    Added

--- js/prod.js
+++ js/prod.js
@@ -4,18 +4,17 @@ async function fetchData(){
         const products= await res.json();
         str=``
         products.products.map((product)=>{
-            str+=`<div class="card">
+            str+=`<div class="cardx">
                     <img src="${product.thumbnail}" alt="">
-                    <h5>${product.title.substring(0,15)}</h5>
-                    <p>${product.price}</p>
                     <button class="bt2"><a href="product.html">Click Here</a></button>
 
             </div>`
         })
-        document.getElementById("cards").innerHTML=str;
+        document.getElementById("c1").innerHTML=str;
     }
     catch(error)
     {
         console.log(error);
     }
 }
+ fetchData();
