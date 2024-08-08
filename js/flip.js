8dda171f2d762ef80ae7020ef01684f2ce5056e1
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-08-08 19:50:33 +0530

    Added

---
+++ js/flip.js
@@ -0,0 +1,47 @@
+     async function fetchData(){
+    try{
+        const res=await fetch("https://dummyjson.com/products");
+        const products= await res.json();
+        str=``
+        products.products.map((product)=>{
+            str+=`<div class="card">
+                    <img src="${product.thumbnail}" alt="">
+                    <h5>${product.title.substring(0,15)}</h5>
+                    <p>${product.price}</p>
+                    <button class="bt2"><a href="product.html">Click Here</a></button>
+
+            </div>`
+        })
+        document.getElementById("cards").innerHTML=str;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+    document.getElementById("search").addEventListener('keyup',async(e)=>{
+        console.log(e.target.value);
+        try{
+            const res=await fetch("https://dummyjson.com/products");
+        const products= await res.json();
+        console.log(products.products);
+        str=``
+        products.products.filter((i)=>i.title.toLowerCase().includes(e.target.value.toLowerCase())).map((product)=>{
+            str+=`<div class="card">
+                    <img src="${product.thumbnail}" alt="">
+                    <h5>${product.title.substring(0,15)}</h5>
+                    <p>${product.price}</p>
+                    <button class="bt2"><a href="product.html">Click Here</a></button>
+
+            </div>`
+        })
+        document.getElementById("cards").innerHTML=str;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+        
+    })
+    
+}
+fetchData();
---
+++ js/prod.js
@@ -0,0 +1,20 @@
+async function fetchData(){
+    try{
+        const res=await fetch("https://dummyjson.com/products");
+        const products= await res.json();
+        str=``
+        products.products.map((product)=>{
+            str+=`<div class="card">
+                    <img src="${product.thumbnail}" alt="">
+                    <h5>${product.title.substring(0,15)}</h5>
+                    <p>${product.price}</p>
+                    <button class="bt2"><a href="product.html">Click Here</a></button>
+
+            </div>`
+        })
+        document.getElementById("cards").innerHTML=str;
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
