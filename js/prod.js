5325084c03b4aaa8455ae080a4264269f4274884
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-08-09 13:44:35 +0530

    added

--- js/prod.js
+++ js/prod.js
@@ -7,30 +7,19 @@ async function fetchData(){
         console.log(id);
         const products= await res.json();
         console.log(products.images);
-        document.getElementById("img").src=products.thumbnail
-        str=``
-            str+=`
-           <div class="cardx">
-            <h1>${products.products.id.title}</h1>
-            <h3>${products.products.id.description}</h3>
-            <h3>${products.products[0].category}</h3>
-            <div class="price"><h1>$${products.products[0].price}</h1></div>
-            <p><b>Brand: </b>${products.products[0].brand}</p>
-            <p><b>sku: </b>${products.products[0].sku}</p>
-            <p><b>Warranty Information: </b>${products.products[0].warrantyInformation}</p>
-            <p><b>Shipping Information: </b>${products.products[0].shippingInformation}</p>
-            <p><span class="r">Rating: ${products.products[0].rating}</span><br><br>Stock: ${products.products[0].stock}<br><br> Availibility: <span class="avail">${products.products[0].availabilityStatus}</span></p>
-             <h3 align="center"><u>Reviews</u></h3>
-            <div class="rev">
-                <div class="review">
-                    <p><b>Name:</b>${products.products[0].reviews[0].reviewerName}</p>
-                    <p><b>Email:</b>${products.products[0].reviews[0].reviewerEmail}</p>
-                    <p> <b>Date:</b>${products.products[0].reviews[0].date}</p>
-                    <p><b>Comment:</b>${products.products[0].reviews[0].comment}</p>
-                </div>
-            </div>
-        </div>`
-        document.getElementById("c1").innerHTML=str;
+        document.getElementById("img").src=`${products.thumbnail}`
+        document.getElementById("one").innerHTML=`${products.title}`
+        document.getElementById("three").innerHTML=`${products.description}`
+        document.getElementById("four").innerHTML=`${products.category}`
+        document.getElementById("five").innerHTML=`${products.price}`
+        document.getElementById("six").innerHTML=`${products.brand}`
+        document.getElementById("seven").innerHTML=`${products.sku}`
+        document.getElementById("eight").innerHTML=`${products.warrantyInformation}`
+        document.getElementById("nine").innerHTML=`${products.shippingInformation}`
+        document.getElementById("ten").innerHTML=`<span class="r">Rating: ${products.rating}</span><br><br>Stock: ${products.stock}<br><br> Availibility: <span class="avail">${products.availabilityStatus}</span>`
+
+        
+        // document.getElementById("c1").innerHTML=str;
     }
     catch(error)
     {
