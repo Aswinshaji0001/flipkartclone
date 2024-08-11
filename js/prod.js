ce9a5bfb5092b3f518894498c0b167ba58e60af2
Aswinshaji0001 <aswinshaji0001@gmail.com>
2024-08-11 22:02:56 +0530

    added

--- js/prod.js
+++ js/prod.js
@@ -6,6 +6,8 @@ async function fetchData(){
         const res=await fetch(`https://dummyjson.com/products/${id}`)
         console.log(id);
         const products= await res.json();
+        console.log(products);
+        
         console.log(products.images);
         document.getElementById("img").src=`${products.thumbnail}`
         document.getElementById("one").innerHTML=`${products.title}`
@@ -17,9 +19,19 @@ async function fetchData(){
         document.getElementById("eight").innerHTML=`${products.warrantyInformation}`
         document.getElementById("nine").innerHTML=`${products.shippingInformation}`
         document.getElementById("ten").innerHTML=`<span class="r">Rating: ${products.rating}</span><br><br>Stock: ${products.stock}<br><br> Availibility: <span class="avail">${products.availabilityStatus}</span>`
-
-        
-        // document.getElementById("c1").innerHTML=str;
+        str=``
+        products.reviews.map((i)=>{
+           str+= `<div class="review">
+                    <p><b>Name:</b>${i.reviewerName}</p>
+                    <p><b>Email:</b>${i.reviewerEmail}</p>
+                    <p> <b>Date:</b>${i.date.substring(0,10)}</p>
+                    <p><b>Comment:</b>${i.comment}</p>
+                </div>`
+        })
+        document.getElementById("rev").innerHTML=str;
+        document.getElementById("pc").innerHTML=`<p> <b> RETURN POLICY : </b> ${products.returnPolicy}</p>
+        <p><b>Minimum Order Quantity :</b>${products.minimumOrderQuantity}</p>`;
+        document.getElementById("img2").src=`${products.meta.qrCode}`;
     }
     catch(error)
     {
